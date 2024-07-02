1e889e259c530025ec00746dee434c333615f91f
Tushar <tusharkaler450@gmail.com>
2024-07-02 18:25:33 +0530

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import About from "./About/About";
 import Projects from "./Projects/Projects";
 import Contact from "./Contact/Contact";
 
-export default function home() {
+export default function Root() {
   const [currPage, setCurrPage] = useState<number>(0);
   return (
     <div className="w-screen h-screen flex">
